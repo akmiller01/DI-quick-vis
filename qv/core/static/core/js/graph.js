5bbe7be252b9c9535ba0116002287452184cf827
Alex Miller <alex.k.miller@gmail.com>
2015-07-20 13:45:34 +0100

    Slight improvements and offline deps

--- qv/core/static/core/js/graph.js
+++ qv/core/static/core/js/graph.js
@@ -1,3 +1,85 @@
+//Color scales
+var shortColor = d3.scale.ordinal()
+.range(
+    ["#ba0c2f" //Red
+    ,"#1b365d" //blue
+    ,"#ea7600" //Orange
+    ,"#93328e" //purple
+    ,"#0095c8" //lightblue
+    ,"#b7bf10"] //Yellow
+);
+
+var longColor = d3.scale.ordinal()
+.range(
+    ['#00688c'
+    ,'#be84bb'
+    ,'#662363'
+    ,'#b7bf10'
+    ,'#0c192d'
+    ,'#1b365d'
+    ,'#cceaf4'
+    ,'#f2ad66'
+    ,'#ea7600'
+    ,'#0095c8'
+    ,'#595d07'
+    ,'#66bfde'
+    ,'#471845'
+    ,'#e9d6e8'
+    ,'#820820'
+    ,'#93328e'
+    ,'#004862'
+    ,'#f7f2cf'
+    ,'#80850b'
+    ,'#f7ced5'
+    ,'#d4d970'
+    ,'#d1d7df'
+    ,'#122541'
+    ,'#76869e'
+    ,'#5b0516'
+    ,'#fbe4cc'
+    ,'#ba0c2f'
+    ,'#723900'
+    ,'#d66d82'
+    ,'#a35200']
+);
+
+//Number formatting
+function abbrNum(number, decPlaces) {
+    // 2 decimal places => 100, 3 => 1000, etc
+    decPlaces = Math.pow(10,decPlaces);
+
+    // Enumerate number abbreviations
+    var abbrev = [ "k", "m", "b", "t" ];
+
+    // Go through the array backwards, so we do the largest first
+    for (var i=abbrev.length-1; i>=0; i--) {
+
+        // Convert array index to "1000", "1000000", etc
+        var size = Math.pow(10,(i+1)*3);
+
+        // If the number is bigger or equal do the abbreviation
+        if(size <= number) {
+             // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+             // This gives us nice rounding to a particular decimal place.
+             number = Math.round(number*decPlaces/size)/decPlaces;
+
+             // Handle special case where we round up to the next abbreviation
+             if((number == 1000) && (i < abbrev.length - 1)) {
+                 number = 1;
+                 i++;
+             }
+
+             // Add the letter for the abbreviation
+             number += abbrev[i];
+
+             // We are done... stop
+             break;
+        }
+    }
+
+    return number;
+};
+
 //Tooltip functions
 $(document).ready(function(){
     $("body").append("<div class='tooltip' id='tooltip' style='position:absolute;background-color: rgb(255, 255, 255); border: 1px solid rgb(128, 128, 128); opacity: 0.9; padding: 1px 5px;'></div>");
@@ -151,7 +233,6 @@ function bar(conf){
 			max: yearMax,
 			step: 1,
 			slide: function( event, ui ) {$( "#year" ).val( ui.value );}});
-        $('#slider').slider({animate: 1000 });
         var timeouts = [];
         play = function(){
             for(var timeout in timeouts){
@@ -209,6 +290,7 @@ function bar(conf){
         //.tickFormat(function(d) {
         //    return logFormat(d)
         //})
+        .tickFormat(function(d){return abbrNum(d, 2)})
         .tickPadding(15);
     
         yAxisContainer.transition().duration(500)
@@ -246,7 +328,9 @@ function bar(conf){
                 x: function (d, i) {
                     return i * barWidth + i * padding;
                 },
-                class: "bar"
+                class: "bar",
+                
+                fill: function(d, i) {return shortColor(0)}
             })
 
         bars.exit().remove();
