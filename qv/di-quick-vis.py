55c767d1a541f8ea0a051300ea5f8057d6e8822c
Alex Miller <alex.k.miller@gmail.com>
2015-07-22 14:45:30 +0100

    Windows build...absolute path

--- qv/di-quick-vis.py
+++ qv/di-quick-vis.py
@@ -1,6 +1,17 @@
 import webbrowser
 import sys
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qv.settings")
+import Cookie
+import htmlentitydefs
+import HTMLParser
+import core
+import core.views
+import core.admin
+import core.forms
+import core.utils
+import core.models
+
 from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
@@ -9,7 +20,8 @@ if __name__ == "__main__":
     except:
         url = "127.0.0.1:8000"
         
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qv.settings")
+    from django.conf import settings
+    print(settings.DATABASES)
     
     fullUrl = "http://"+url
     webbrowser.open(fullUrl)
